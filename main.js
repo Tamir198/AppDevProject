e50cf791b8867d8ef7aa0e0b2e64e6b30aadebbf
Tamir <1tamir198@gmail.com>
2022-04-27 11:52:56 +0300

    Finished exe 1

--- main.js
+++ main.js
@@ -1,28 +1,43 @@
 import data from './db.js'
 import dateUtils from "/utils/dates.js"
 
-$(document).ready(() => {
+const adsToShow = [];
 
-  var date = new Date();
-   data.forEach(ad => {
+const showAd = (ad) => {
+  $("#ads__container").load(ad.templateUrl, () => {
+    $('h1').replaceWith(`<h1>${ad.name}</h1>`);
+    $("#ads__container").append(`<p>${ad.texts}</p>`);
+
+    ad.imagesUrl.forEach(item => {
+      $("#ads__container").append(`<img src="${item}" height: 100px; width="200px" alt="Image not found" />`);
+    })
+  });
+}
+
+$(document).ready(() => {
+  const date = new Date();
+  data.forEach(ad => {
     ad.timesToShow.forEach(timeToShow => {
-      var fromDate = new Date(timeToShow.fromDate);
-      var toDate = new Date(timeToShow.toDate);
+      const fromDate = new Date(timeToShow.fromDate);
+      const toDate = new Date(timeToShow.toDate);
 
       if (dateUtils.isBetweenDates(date, fromDate, toDate)) {
         if (dateUtils.shouldShowAdNow(date, timeToShow.days)) {
-          console.log(ad);
-          
-          $("#ads__container").load(ad.templateUrl, ()=>{
-            $('h1').replaceWith(`<h1>${ad.name}</h1>`);
-            $("#ads__container").append(`<p>${ad.texts}</p>`);
-            
-            ad.imagesUrl.forEach(item => {
-              $("#ads__container").append(`<img src="${item}" height: 100px; width="200px" alt="Image not found" />`);
-            })
-          });
+          adsToShow.push(ad);
         }
       }
     })
   });
+
+  let adToShowIndex = 0;
+  setInterval(() => {
+    showAd(adsToShow[adToShowIndex]);
+    if (adToShowIndex == adsToShow.length-1) {
+      adToShowIndex = 0;
+    } else {
+      adToShowIndex++;
+    }
+  }, 1500);
+
 });
+
